41d4509c19c8ca21af5a0117d874ea3975fa680d
Ryan Schmitt <rschmitt@pobox.com>
2025-11-25 13:53:03 -0800

    text_spec: Replace deprecated `should` syntax with `expect`

--- spec/text_spec.rb
+++ spec/text_spec.rb
@@ -3,12 +3,12 @@ require_relative "spec_helper"
 describe Text do
   describe "truncating" do
     it "can truncate itself to a given width" do
-      Text["this is long"].truncate_to_width(6).should == Text["this i"]
+      expect(Text["this is long"].truncate_to_width(6)).to eq(Text["this i"])
     end
 
     it "sometimes leaves empty strings behind when truncating" do
       text = Text[:red, "one", :green, "two", :blue, "three"]
-      text.truncate_to_width(2).should == Text[:red, "on", :green, "", :blue, ""]
+      expect(text.truncate_to_width(2)).to eq(Text[:red, "on", :green, "", :blue, ""])
     end
   end
 end
